fe42395a7e12391f1531b04b20d6ba69da479264
LJH <95152341+wkjdak@users.noreply.github.com>
2023-05-17 13:47:12 +0900

    Add files via upload

---
+++ 2023-05-12/js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('.topBanner .inner i').on('click', function () {
+
+        $('.topBanner ').addClass('on')
+
+
+
+
+    })
+
+
+})
+
+$(function () {
+
+    // slick 실행 스크립트
+    $('.mainSlide').slick();
+    // arrows: false,
+    // dots: true,
+
+});
